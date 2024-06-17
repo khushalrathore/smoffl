a9e7141f25e70fa2233f58b53a83c7d742e86abd
Khushal <25khushal+github@gmail.com>
2024-06-17 20:09:16 +0530

    Day 1

--- src/Components/Sections/Hero.jsx
+++ src/Components/Sections/Hero.jsx
@@ -3,7 +3,7 @@ import HeroTile from './HeroTile';
 const heroItem = {
   first: {
     Img: 'https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcGR2YW5nb2doLXNlbGYtcG9ydHJhaXQtbTAxLWpvYjY2MV8yLWwxMDBvNmVmLmpwZw.jpg',
-    Accent: 'yellow'
+    Accent: 'black'
   }
 }
 const desc1 = 'Unlock your creative potential with our comprehensive video editing courses. Learn advanced techniques and transform your ideas into stunning visuals.'
@@ -13,7 +13,7 @@ function Hero() {
     <>
       <div className={styles.heroDiv}>
         <div className={styles.heroDivBg} style={{ background: `linear-gradient(${heroItem.first.Accent} 0%, rgba(255, 255, 255, 0.006) 90%)` }}></div>
-        <div className={styles.heroDiv1}>
+        <div className={styles.heroDiv1} >
           <div className={styles.mini} style={{ background: `url(${heroItem.first.Img})no-repeat center center` }}></div>
           <span className={styles.title}>Premiere Pro 101</span>
           <p className={styles.description}>{desc1}</p>
