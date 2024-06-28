5f141c1b9268ba56cf74b34d01d0cf6581c5c791
Khushal <25khushal+github@gmail.com>
2024-06-28 19:23:13 +0530

    Day9

--- src/Pages/Courses/Courses.jsx
+++ src/Pages/Courses/Courses.jsx
@@ -2,25 +2,29 @@ import { coursesInfo } from '../0/PagesData';
 import styles from './Courses.module.css';
 import CourseListItem from './CourseListItem';
 import CourseItemDescription from './CourseItemDescription';
+import '../../../globals.css';
 
 function Courses() {
   return (
-    <div className={styles.coursesDiv}>
-      <div className={styles.coursesDivHeading}>
-        <div>
-          <span className={styles.newCourses}>New Courses</span>
-          <span className={styles.allCourses}>All Courses</span>
+    <>
+      <div className="pageDiv">
+        <div className="pageTitle">
+          <span>Courses</span>
+        </div>
+        <div className={styles.coursesDiv}>
+
+          <div className={styles.coursesDivContent}>
+            <div className={styles.coursesList}>
+              {coursesInfo.map((member) => <CourseListItem key={member.id} Title={member.title} Image={member.icon} Stats={member.time} />)}
+            </div>
+            {/* <div className={styles.courseDesc}>
+              {coursesInfo.map((member) => <CourseItemDescription key={member.id} Title={member.title} Image={member.icon} Desc={member.desc} />)}
+            </div> */}
+          </div>
         </div>
       </div>
-      <div className={styles.coursesDivContent}>
-        <div className={styles.coursesList}>
-          {coursesInfo.map((member) => <CourseListItem key={member.id} Title={member.title} Image={member.icon} Stats={member.time} />)}
-        </div>
-        <div className={styles.courseDesc}>
-          {coursesInfo.map((member) => <CourseItemDescription key={member.id} Title={member.title} Image={member.icon} Desc={member.desc} />)}
-        </div>
-      </div>
-    </div>
+
+    </>
   );
 }
 
